cbb355e66114b5e64f1f160855765d4c402953f7
J-designs05 <olasojiife2017@gmail.com>
2023-03-14 11:57:16 +0100

    First Verson

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function nav_open() {
+    document.getElementById("navBarBox2").style.display = "block";
+}
+
+function nav_close() {
+    document.getElementById("navBarBox2").style.display = "none";
+}
