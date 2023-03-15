07ce85356dc576d0d889b69ee1305851a81e3287
J-designs05 <olasojiife2017@gmail.com>
2023-03-15 19:25:10 +0100

    new version with preloader

--- script/script.js
+++ script/script.js
@@ -5,3 +5,10 @@ function nav_open() {
 function nav_close() {
     document.getElementById("navBarBox2").style.display = "none";
 }
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function() {
+  setTimeout(function(){
+    loader.style.display = "none";
+  }, 10000);
+});
